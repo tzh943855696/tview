47fb25a5267182e783979cad7bea7e39daa1882b
Viking Zhang <vikingmute@gmail.com>
2023-02-17 19:14:21 +0800

    Button 组件编码第一部分

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VkButton'
+})
+</script>
+<script setup lang="ts">
+// import type { ButtonProps } from './types'
+import { buttonProps } from './types'
+defineProps(buttonProps)
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Button/types.ts
@@ -0,0 +1,33 @@
+import type { PropType } from 'vue'
+export type ButtonType = 'primary'| 'success'| 'warning'| 'danger'| 'info'
+export type ButtonSize = 'large' | 'small'
+
+export interface ButtonProps {
+  type?: ButtonType;
+  size?: ButtonSize;
+  plain?: boolean;
+  round?: boolean;
+  circle?: boolean;
+  disabled?: boolean;
+}
+
+export const buttonProps = {
+  type: {
+    type: String as PropType<ButtonType>
+  },
+  size: {
+    type: String as PropType<ButtonSize>,
+  },
+  plain: {
+    type: Boolean
+  },
+  round: {
+    type: Boolean
+  },
+  circle: {
+    type: Boolean
+  },
+  disabled: {
+    type: Boolean
+  },
+}
