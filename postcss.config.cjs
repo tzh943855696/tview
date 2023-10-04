b8e7bcf9e606a8d83216ede4b3b852dab9e02020
locke <943855696@qq.com>
2023-10-04 15:07:56 +0800

    tview-button

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,5 +2,6 @@
 module.exports = {
   plugins: [
     require('postcss-nested'),
+    require('postcss-each')
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import Button from './components/Button/Button.vue'
+import Collapse from './components/Collapse/Collapse.vue'
+import Item from './components/Collapse/CollapseItem.vue'
 import type { ButtonInstance } from './components/Button/types'
 const buttonRef = ref<ButtonInstance | null>(null)
 onMounted(() => {
@@ -16,13 +18,41 @@ onMounted(() => {
   </header>
 
   <main>
-    <Button type="primary" plain disabled ref="buttonRef">Test Button</Button>
+    <Button ref="buttonRef">Test Button</Button>
+    <Button plain>Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>VK</Button>
+    <Button disabled>Disabled Button</Button><br/><br/>
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br/><br/>
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br/><br/>
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button>
+
+    <Collapse>
+      <Item name="a" >
+        <template #title>
+          <h1>nice title</h1>
+        </template>
+        <h1>headline title</h1>
+        <div> this is content a aaa </div>
+      </Item>
+      <Item name="b" title="nice title b item b">
+        <div> this is bbbbb test </div>
+      </Item>
+      <Item name="c" title="nice cccc" disabled>
+        <div> this is cccc test </div>
+      </Item>
+    </Collapse>
+
   </main>
-  <h1>hello world</h1>
-  <h2>hello world</h2>
-  <h3>hello world</h3>
-  <a href="#">The link</a>
-  <hr/>
 </template>
 
 <style scoped>
